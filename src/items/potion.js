323c843d4888c5f0fc2f44a66b7367f8cf90f628
Menencia <menencia@gmail.com>
2014-06-23 23:41:21 +0200

    Uppercase items name

--- src/items/potion.js
+++ src/items/potion.js
@@ -3,7 +3,7 @@ class Potion extends Item {
     constructor(game) {
         super(game);
 
-        this.name = 'Potion';
+        this.name = 'Potion'.toUpperCase();
 
         this.heal = 150;
     }
